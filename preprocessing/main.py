1aa526604a324c939e8061bef69a693945fae112
ririgi <lrpbaltazar@gmail.com>
2019-07-08 05:49:15 +0800

    try single file only

--- preprocessing/main.py
+++ preprocessing/main.py
@@ -14,10 +14,11 @@ download_dir = '../downloaded'
 data_dir = 'ProdDataHub/TransactionFactTable/IWant/2018/12'
 
 def main(file, f):
+	s = time.time()
 	quali_out = os.path.join("../../data/quali/"+month, f[-12:])
 	quanti_out = os.path.join("../../data/quanti/"+month, f[-12:])
 	month_out = os.path.join("../../data/month/"+month, f[-12:])
-	pool = Pool(maxtasksperchild = 1)
+	pool = Pool()
 	df = pool.apply(getQualitative, args = (file, ["gigyaid", "devicetype", "deviceos", "browsertype",
 															"connectivitytype", "devicename", "mobiledevice",
 															"screensize", "videoquality", "ipaddress"]))
@@ -28,26 +29,29 @@ def main(file, f):
 	toCSV(df, month_out)
 	pool.close()
 	pool.join()
+	e = time.time()
+	total_time = time.strftime("%H:%M:%S", time.gmtime(e-s))
+	print("Total process time: ", total_time)
 
 if __name__ == '__main__':
-	token = lib.auth()
-	adl = core.AzureDLFileSystem(token, store_name = 'bigdatadevdatalake')
+	# token = lib.auth()
+	# adl = core.AzureDLFileSystem(token, store_name = 'bigdatadevdatalake')
 
-	for f in adl.ls(data_dir):
-		s = time.time()
-		print("Processing file {}".format(f[-38:]))
-		outfile = os.path.join(download_dir, f[-38:])
-		downloader = multithread.ADLDownloader(adl, f, outfile)
-		if downloader.successful():
-			print("Finished Downloading!")
-			main(outfile, f)
-			os.remove(outfile)
-			e = time.time()
-			total_time = time.strftime("%H:%M:%S", time.gmtime(e-s))
-			print("Finished downloading: ", total_time)
-		else:
-			print("error in downloading!")
-
+	# for f in adl.ls(data_dir):
+	# 	s = time.time()
+	# 	print("Processing file {}".format(f[-38:]))
+	# 	outfile = os.path.join(download_dir, f[-38:])
+	# 	downloader = multithread.ADLDownloader(adl, f, outfile)
+	# 	if downloader.successful():
+	# 		print("Finished Downloading!")
+	# 		main(outfile, f)
+	# 		os.remove(outfile)
+	# 		e = time.time()
+	# 		total_time = time.strftime("%H:%M:%S", time.gmtime(e-s))
+	# 		print("Finished downloading: ", total_time)
+	# 	else:
+	# 		print("error in downloading!")
+	main('../downloaded/IWantTransactionFactTable-20181201.csv', 'ProdDataHub/TransactionFactTable/IWant/2018/12/IWantTransactionFactTable-20181201.csv')
 	# for f in os.listdir("../10"):
 	# 	file = os.path.join("../10", f)
 	# 	main(file, f)
