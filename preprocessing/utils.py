34eb5aa5da809a5ec33c4d396f4e85bce530ff13
ririgi <lrpbaltazar@gmail.com>
2019-07-08 09:27:04 +0800

    edit default values for readCSV and readChunk

--- preprocessing/utils.py
+++ preprocessing/utils.py
@@ -6,15 +6,16 @@ import time
 import numpy as np
 import pandas as pd
 
-def readCSV(file, usecols, converters = None, encoding = None):
-	df = pd.read_csv(file, usecols = usecols, dtype = str, 
+def readCSV(file, usecols=None, converters = None, encoding = None, dtype = str):
+	df = pd.read_csv(file, usecols = usecols, dtype = dtype, 
 		low_memory = False, converters = converters, encoding = encoding)
+	df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
 	return df
 
 def toCSV(df, filename, index = True, encoding = None):
 	df.to_csv(filename, index = index, sep = ',', encoding = encoding)
 
-def readChunk(file, usecols, chunksize = 5000000, iterator = True):
+def readChunk(file, usecols=None, chunksize = 5000000, iterator = True):
 	s = time.time()
 	df = pd.read_csv(file, usecols = usecols, dtype = str, low_memory = False,
 		chunksize = chunksize, iterator = iterator)
